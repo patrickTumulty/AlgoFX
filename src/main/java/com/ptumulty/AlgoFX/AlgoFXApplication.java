2ff75cdbd30bc9b72a0cb34c5087f81b9847b67a
ptumulty <ptumulty1@gmail.com>
2022-07-08 22:07:56 -0700

    Initial commit

---
+++ src/main/java/com/ptumulty/AlgoFX/AlgoFXApplication.java
@@ -0,0 +1,19 @@
+package com.ptumulty.AlgoFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AlgoFXApplication extends Application
+{
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+    }
+}
